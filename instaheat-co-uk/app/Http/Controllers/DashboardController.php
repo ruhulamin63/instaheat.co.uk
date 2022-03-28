2e10fda09f6dcbd0f9c8bd06c3f580d70bc335b9
Ruhul Amin <raridoy4@gmail.com>
2022-03-29 01:46:04 +0600

    update for instaheat project

---
+++ instaheat-co-uk/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function customer_index(){
+        //dd("test");
+        return view('customer-view.customerIndex');
+    }
+}
