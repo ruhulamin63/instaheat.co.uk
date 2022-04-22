56b74e908f057d623c7c9d721019582ff85d3ec9
ruhulamin63 <raridoy4@gmail.com>
2022-04-22 16:53:51 +0600

    pici

--- resources/views/customer-view/index.blade.php
+++ resources/views/customer-view/index.blade.php
@@ -16,7 +16,7 @@
               <h1>Plumbing and Heating Engineers</h1>
               
               <div class="sc-gZMcBi gJQWhe">
-                <a class="primary-cta" href="{{route('customer.get.quote')}}">Test</a>
+                <a class="primary-cta" href="{{route('customer.get.quote')}}">pici</a>
               </div>
             </div>
           </div>
